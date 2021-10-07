4b0025568a186794344641dabaf78d993d1cc618
Frank Bokdam <frank@vendic.nl>
2021-10-07 11:03:49 +0200

    IAI-701 copy deeple translations

--- Model/Translator/Catalog/Product.php
+++ Model/Translator/Catalog/Product.php
@@ -6,32 +6,41 @@
  * @copyright 2018 aromicon GmbH (http://www.aromicon.de)
  * @license   Commercial https://www.aromicon.de/magento-download-extensions-modules/de/license
  */
+
 namespace Aromicon\Deepl\Model\Translator\Catalog;
+
+use Aromicon\Deepl\Api\TranslatorInterface;
+use Aromicon\Deepl\Helper\Config;
+use Exception;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
+use Magento\Framework\Exception\LocalizedException;
 
 class Product
 {
     /**
-     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     * @var ProductRepositoryInterface
      */
     private $productRepository;
 
     /**
-     * @var \Aromicon\Deepl\Api\TranslatorInterface
+     * @var TranslatorInterface
      */
     private $translator;
 
     /**
-     * @var \Aromicon\Deepl\Helper\Config
+     * @var Config
      */
     private $config;
 
     private $productResource;
 
     public function __construct(
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        \Aromicon\Deepl\Api\TranslatorInterface $translator,
-        \Aromicon\Deepl\Helper\Config $config,
-        \Magento\Catalog\Model\ResourceModel\Product $productResource
+        ProductRepositoryInterface $productRepository,
+        TranslatorInterface $translator,
+        Config $config,
+        ProductResource $productResource
     ) {
         $this->productRepository = $productRepository;
         $this->translator = $translator;
@@ -43,8 +52,8 @@ class Product
      * @param $productId int
      * @param $fromStoreId int
      * @param $toStoreId int
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @throws \Exception
+     * @throws LocalizedException
+     * @throws Exception
      */
     public function translateAndCopy($productId, $toStoreId)
     {
@@ -69,7 +78,15 @@ class Product
             }
 
             $product->setData($field, $translatedText);
-            $this->productResource->saveAttribute($product, $field);
+            $this->saveAttribute($product, $field);
         }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function saveAttribute(ProductInterface $product, string $field): ProductResource
+    {
+        return $this->productResource->saveAttribute($product, $field);
     }
 }
